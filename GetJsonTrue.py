76eee932dbcee5daa89a7ab47e3d82150b2f08e5
Viransh Shah <shahv47@mcmaster.ca>
2024-12-20 20:18:18 -0500

    update get true

--- GetJsonTrue.py
+++ GetJsonTrue.py
@@ -7,7 +7,7 @@ def main():
     printed_urls = set()  # To avoid duplicate prints
     for dictionary in data:
         for chapter in dictionary["results"]:
-            if any(value is True for value in chapter["found_terms"].values()):
+            if any(value is True for term, value in chapter["found_terms"].items() if term.startswith("f")):
                 if chapter["chapter_url"] not in printed_urls:
                     print(chapter["chapter_url"])
                     printed_urls.add(chapter["chapter_url"])
