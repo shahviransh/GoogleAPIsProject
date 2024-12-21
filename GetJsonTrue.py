542db218ae69aa786a555e2daf9a37b196b9b618
Viransh Shah <shahv47@mcmaster.ca>
2024-12-20 22:34:31 -0500

    update

--- GetJsonTrue.py
+++ GetJsonTrue.py
@@ -1,17 +1,23 @@
 import json
 
+
 def main():
-    with open('results.json', 'r') as f:
+    with open("results.json", "r") as f:
         data = json.load(f)
-    
+
     printed_urls = set()  # To avoid duplicate prints
     for dictionary in data:
         for chapter in dictionary["results"]:
-            if all(value is True for term, value in chapter["found_terms"].items() if term.startswith("f")):
+            if all(
+                value
+                for term, value in chapter["found_terms"].items()
+                if term.startswith("f") and value is True
+            ):
                 if chapter["chapter_url"] not in printed_urls:
                     print(chapter["chapter_url"])
                     printed_urls.add(chapter["chapter_url"])
                 break  # Exit the loop early as we found a true value for this novel
+
 
 if __name__ == "__main__":
     main()
